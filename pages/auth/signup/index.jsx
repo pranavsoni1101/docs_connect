f0b755b2d0b2842f981ee68ad5fe0b6550375db9
Pranav Soni <pranavsoni1101@gmail.com>
2022-06-15 21:25:34 +0530

    Changed the sign up form and deleted unecessary files

--- pages/auth/signup/index.jsx
+++ pages/auth/signup/index.jsx
@@ -1,12 +1,20 @@
 
 import { useState } from 'react';
-import ReviewInfo from '../../../components/SignUp/ReviewInfo';
-import UserDetails from '../../../components/SignUp/UserDetails';
-import DoctorDetails from '../../../components/SignUp/DoctorDetails';
+import { Center, Box, Heading, 
+    FormControl, FormLabel, Input, 
+    InputGroup, InputRightElement, Button,
+    Text, FormErrorMessage, HStack,
+    Stack, Grid,    
+    GridItem
+} from '@chakra-ui/react';
+import Link from "next/link";
+import { useForm } from 'react-hook-form';
+import Section from '../../../components/Section';
+import SectionContent from '../../../components/Section/SectionContent';
 
 const LogIn = () => {
 
-    const [step, setStep] = useState(1);
+    const { register, formState: {errors, isValid}, watch} = useForm();
     const [state, setState] = useState({
         name: "",
         email: "",
@@ -16,15 +24,6 @@ const LogIn = () => {
         specialization: "",
         show: false,
     });
-
-    const prevStep = () => {
-        if(step !== 0)
-            setStep(step - 1);
-    };
-
-    const nextStep = () => {
-        setStep(step + 1);
-    };
 
     const handleInputChange = (event) => {
         setState({
@@ -45,65 +44,145 @@ const LogIn = () => {
     }
 
     const inputType = state.show ? 'text' : 'password';
-    
-    const renderFormStep = 
-                            step === 0 ? 
-                                <UserDetails 
-                                    state              = {state}
-                                    nextStep           = {nextStep}
-                                    inputType          = {inputType}
-                                    handleInputChange  = {handleInputChange}
-                                    handlePasswordShow = {handlePasswordShow}
+    return(
+        <Section>
+            <SectionContent>    
+                <Heading
+                    size         = "lg"
+                    color        = "brand1.500"
+                    marginBottom = "1em"
+                >
+                    Register Yourself
+                </Heading>
+                <form>
+                    <FormControl>
+                        <Grid
+                            templateColumns = "repeat(12, 1fr)"
+                            gap={4}
+                        >
+                            <GridItem
+                                colSpan={6}
+                            >
+                                <FormLabel htmlFor="name">Name</FormLabel>
+                                <Input 
+                                    id           = "name"
+                                    name         = "name"
+                                    type         = "text"
+                                    value        = {state.name}
+                                    variant      = "filled"                                    
+                                    onChange     = {handleInputChange}
+                                    placeholder  = 'Enter your full name'
+                                    autoComplete = "off"
+                                    marginBottom = "1em"
                                 />
-                                : step === 1 ?
-                                <DoctorDetails 
-                                    state             = {state}
-                                    prevStep          = {prevStep}
-                                    nextStep          = {nextStep}
-                                    handleInputChange = {handleInputChange}
+                            </GridItem>
+                            <GridItem
+                                colSpan={6}
+                            >
+                                <FormLabel htmlFor="email">Email</FormLabel>
+                                <Input 
+                                    id           = "email"
+                                    name         = "email"
+                                    type         = "email"
+                                    // value        = {state.email}
+                                    variant      = "filled"                                    
+                                    // onChange     = {handleInputChange}
+                                    placeholder  = 'Enter email'
+                                    autoComplete = "off"
+                                    marginBottom = "1em"
+                                    {...register('email', {
+                                        required: "Email cannot be empty",
+                                        pattern: /^\S+@\S+$/i
+                                    })}
                                 />
-                            :
-                                <ReviewInfo 
-                                    state        = {state}
-                                    prevStep     = {prevStep}
-                                    handleSubmit = {handleSubmit}
-                                />
-                               
-    
-    switch(step) {
-        case 1: return(
-            <UserDetails 
-                state              = {state}
-                nextStep           = {nextStep}
-                inputType          = {inputType}
-                handleInputChange  = {handleInputChange}
-                handlePasswordShow = {handlePasswordShow}
-            />
-        );
-        case 2: return(
-            <DoctorDetails 
-                state             = {state}
-                prevStep          = {prevStep}
-                nextStep          = {nextStep}
-                handleInputChange = {handleInputChange}
-            />
-        )
-        case 3: return(
-            <ReviewInfo 
-                state        = {state}
-                prevStep     = {prevStep}
-                handleSubmit = {handleSubmit}
-            />
-        )
-        default: return(
-            null
-        )
-    }
-    // return(
-    //     <>
-    //         {renderFormStep}   
-    //     </>
-    // )
+                                <FormErrorMessage>
+                                    {errors.email && errors.email.message}
+                                </FormErrorMessage>
+                            </GridItem>
+                            <GridItem
+                                colSpan={6}
+                            >
+                                <FormLabel htmlFor="qualification">Qualification</FormLabel>
+                                    <Input 
+                                        id           = "qualification"
+                                        name         = "qualification"
+                                        type         = "text"
+                                        value        = {state.qualification}
+                                        variant      = "filled"                                    
+                                        onChange     = {handleInputChange}
+                                        placeholder  = 'Qualification'
+                                        autoComplete = "off"
+                                        marginBottom = "1em"
+                                    />
+                            </GridItem>
+                            <GridItem colSpan={6}>
+                            <FormLabel htmlFor="specialization">Specialization</FormLabel>
+                                    <Input 
+                                        id           = "specialization"
+                                        name         = "specialization"
+                                        type         = "text"
+                                        value        = {state.specialization}
+                                        variant      = "filled"                                    
+                                        onChange     = {handleInputChange}
+                                        placeholder  = 'Enter specialization'
+                                        autoComplete = "off"
+                                        marginBottom = "1em"
+                                    />
+                            </GridItem>
+                            <GridItem colSpan={6}>
+                            <FormLabel htmlFor="password">Password</FormLabel>
+                                    <InputGroup>
+                                        <Input
+                                            id           = "password"
+                                            type         = {inputType}
+                                            name         = "password"
+                                            value        = {state.password}
+                                            variant      = "filled"                                    
+                                            onChange     = {handleInputChange}
+                                            placeholder  = 'Enter password'
+                                            marginBottom = "1em"
+                                        />
+                                        <InputRightElement width='4.5rem'>
+                                            <Button 
+                                                size    = 'xs' 
+                                                onClick = {handlePasswordShow}
+                                            >
+                                                {state.show ? 'Hide' : 'Show'}
+                                            </Button>
+                                        </InputRightElement>
+                                    </InputGroup>
+                            </GridItem>
+                            <GridItem colSpan={6}>
+                            <FormLabel htmlFor="repassword">Repeat Password</FormLabel>
+                                <Input
+                                        id           = "repassword"
+                                        type         = "password"
+                                        name         = "repassword"
+                                        value        = {state.repassword}
+                                        variant      = "filled"                                    
+                                        onChange     = {handleInputChange}
+                                        placeholder  = 'Re-enter password'
+                                        marginBottom = "1em"
+                                    />
+                            </GridItem>
+                            <GridItem
+                                colSpan={4}
+                            >
+                                <Button
+                                    type        = 'submit'
+                                    onClick     = {handleSubmit}
+                                    colorScheme = "green"
+                                    width       = "100%"
+                                >
+                                    Submit
+                                </Button>
+                            </GridItem>
+                        </Grid>
+                    </FormControl>
+                </form>
+            </SectionContent>
+        </Section>
+    )
 }
 
 export default LogIn;
